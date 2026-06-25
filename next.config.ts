178b665f4eb2b495f395fbae571875a2aac27fde
Vinil Prabhu <vinilprabhu@gmail.com>
2026-06-25 13:00:18 +0530

    Fix typo in Digital Menu description and update Next.js configuration settings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = process.env.NODE_ENV === "production" ? "/vinilprabhu_next" : "";
+
 const nextConfig: NextConfig = {
-  basePath: "/",
   output: "export",
+  basePath: "",
+  assetPrefix: repoName,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
